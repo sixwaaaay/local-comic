457f030e8f5fc19d4b93c133e78768fddff8fad3
sixwaaaay <lw.2675@qq.com>
2023-12-07 22:05:30 +0800

    Remove unused components and update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <RouterLink to="/comic">Comics</RouterLink>
   </main>
 </template>
